1bd3e5f83332a24afc9a8f36c22747cb7d832db2
nrmarther <nathan@martherus.com>
2023-05-18 21:08:13 -0600

    finished u3 activities

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const env = require("dotenv").config()
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
-
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -25,10 +25,25 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(require("./routes/static"))
 
 // Index route
-app.get ("/", baseController.buildHome)
+app.get ("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
