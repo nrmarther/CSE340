9e3ad8e6e132fa2d4a9faac7b8a9f5a4200aecb7
nrmarther <nathan@martherus.com>
2023-06-27 17:49:56 -0600

    finished assignment 5

--- public/js/inv-update.js
+++ public/js/inv-update.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("#updateForm")
+const form = document.querySelector("#update")
     form.addEventListener("change", function () {
       const updateBtn = document.querySelector("#submit-button")
       updateBtn.removeAttribute("disabled")
